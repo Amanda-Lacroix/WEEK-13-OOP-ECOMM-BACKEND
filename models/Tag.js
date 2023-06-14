a9e887b3be6ad44cb96de75f80028ed7e875c5bd
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-14 19:28:37 -0300

    added models

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
   },
   {
     sequelize,
@@ -18,4 +27,5 @@ Tag.init(
 );
 
 module.exports = Tag;
+
  
